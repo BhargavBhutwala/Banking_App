1bea1f002751fccb50091596fc6146d2295dfc2f
Bhargav Bhutwala <bhargav.bhutwala@ibm.com>
2026-07-28 14:30:25 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/learning/demo/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.learning.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/userApi")
+public class MyController {
+
+    private List<String> users = List.of("user1", "user2", "user3");
+
+    @GetMapping
+    public List<String> getUsers() {
+        return users;
+    }
+}
