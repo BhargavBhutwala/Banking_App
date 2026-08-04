0b50b15fcda22d2fd17dd288580d4daa5aed6307
Bhargav Bhutwala <bhargav.bhutwala@ibm.com>
2026-08-04 14:26:00 +0530

    Add banking application frontend

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import type { Account } from "./models/account";
+
+let account:Account = {
+
+    id: 101,
+    name:  "Bharggav",
+    age: 25,
+    accountNo: "1234567890",
+    balance: 500
+}
---
+++ backend/src/models/account.ts
@@ -0,0 +1,8 @@
+export interface Account{
+
+    id?:number;
+    name?: string;
+    age?: number;
+    accountNo?: string;
+    balance?: number;
+}
